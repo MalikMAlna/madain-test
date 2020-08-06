cfe2357de8f497a3ddcb7c67074a80bedd97c25a
Mike A <jimalik2525@gmail.com>
2020-08-06 05:54:14 -0400

    outlined js plans for functionality, may change as things go on

---
+++ main.js
@@ -0,0 +1,11 @@
+// Set Player Turns
+
+// Draw X or O to board each turn 
+
+// Track Rows and Columns
+
+// Check Rows and Columns for Matches
+
+// Set Row, Column, and Diagonal Victory Conditions
+
+// Reset Game Board, regardless of victory conditions
