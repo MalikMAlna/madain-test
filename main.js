b3dcae2366ea09121a3321d4d6ea5eaee9929bc6
Mike A <jimalik2525@gmail.com>
2020-08-06 06:58:46 -0400

    Able to draw X or O into cells

--- main.js
+++ main.js
@@ -2,26 +2,37 @@
 let gameOver = false
 let player1 = "X";
 let player2 = "O";
-let playerTurn;
+let playerTurn = player1;
 let cell_divs = document.querySelectorAll(".cell")
 let currentPlayer_h1 = document.getElementById("player-turn");
 let results_div = document.getElementById("results");
 
+
+// Draw Mark Functionality
+const drawMark = (cell, currentTurn) => {
+    if(currentTurn === player1) {
+    cell.innerHTML = currentTurn;
+    playerTurn = player2;
+    } else if(currentTurn === player2) {
+        cell.innerHTML = currentTurn;
+        playerTurn = player1;
+    }
+}
+
 // Set Player Turns
 const checkCell = (e) => {
     let cell = e.target;
-    let currentTurn = playerTurn == player1 ? player1 : player2
+    let currentTurn = playerTurn;
     // Draw X or O to board each turn 
+    drawMark(cell, currentTurn);
+    // Change Turns
+    // Check Rows and Columns for Matches
+    // Set Row, Column, and Diagonal Victory Conditions
 }
 
 // Track Rows and Columns
 cell_divs.forEach((cell) =>{
     cell.addEventListener("click", checkCell, {once: true});
 });
-
-
-// Check Rows and Columns for Matches
-
-// Set Row, Column, and Diagonal Victory Conditions
 
 // Reset Game Board, regardless of victory conditions
