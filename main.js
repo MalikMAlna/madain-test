7135f107fd5bb3d60c804d568c0a98249349a79d
Mike A <jimalik2525@gmail.com>
2020-08-06 07:52:03 -0400

    Finished checkWin victory condition check and cleaned up some earlier unnecessary html

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Cached Variables
-let gameOver = false
+let gameOver;
 let player1 = "X";
 let player2 = "O";
 let playerTurn = player1;
@@ -7,27 +7,63 @@ let cell_divs = document.querySelectorAll(".cell")
 let currentPlayer_h1 = document.getElementById("player-turn");
 let results_div = document.getElementById("results");
 
+// Set Row, Column, and Diagonal Victory Conditions
+const POSSIBLE_WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 
 // Draw Mark Functionality
 const drawMark = (cell, currentTurn) => {
     // Change Turns
     if(currentTurn === player1) {
+    cell.classList.add("cell-marked");
+    cell.classList.add("X");
     cell.innerHTML = currentTurn;
-    playerTurn = player2;
+    currentPlayer_h1.innerHTML = "Player 2's Turn";
+    playerTurn = switchTurns(playerTurn);
     } else if(currentTurn === player2) {
+        cell.classList.add("cell-marked");
+        cell.classList.add("O");
         cell.innerHTML = currentTurn;
-        playerTurn = player1;
+        currentPlayer_h1.innerHTML = "Player 1's Turn";
+        playerTurn = switchTurns(playerTurn);
     }
 }
 
 // Set Player Turns
+const switchTurns = (playerTurn) => {
+    if(playerTurn === player1) {
+        return playerTurn = player2
+    } else if(playerTurn === player2) {
+        return playerTurn = player1
+    }
+}
+
 const checkCell = (e) => {
     let cell = e.target;
     let currentTurn = playerTurn;
     // Draw X or O to board each turn 
     drawMark(cell, currentTurn);
     // Check Rows and Columns for Matches
-    // Set Row, Column, and Diagonal Victory Conditions
+    if(checkForWin(currentTurn)) {
+        gameOver = false;
+    }
+}
+
+const checkForWin = (currentTurn) => {
+ return POSSIBLE_WINNING_COMBINATIONS.some(possible_combos => {
+     return possible_combos.every(idx => {
+         return cell_divs[idx].classList.contains(currentTurn);
+     });
+ });
 }
 
 // Track Rows and Columns
@@ -35,4 +71,9 @@ cell_divs.forEach((cell) =>{
     cell.addEventListener("click", checkCell, {once: true});
 });
 
+
+
 // Reset Game Board, regardless of victory conditions
+const resetGame = () => {
+    location.reload();
+}
